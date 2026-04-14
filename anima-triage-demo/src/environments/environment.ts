ab8a9c7e98a03b4da56ac5c4481441848ef71ee1
Yiannis <ioannis.roussos@hotmail.com>
2026-04-15 00:40:01 +0300

    Refactor API Gateway configuration by removing OPTIONS methods and update environment API URL

--- anima-triage-demo/src/environments/environment.ts
+++ anima-triage-demo/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // Replace with your API Gateway URL after running `sam deploy`
   // e.g. https://abc123.execute-api.eu-west-2.amazonaws.com/prod
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://t4svo9ysn0.execute-api.eu-west-2.amazonaws.com/prod',
 };
